cc472cda48d33efa2d781b59121eab5674239b47
ermisethane <79734197+ermisethane@users.noreply.github.com>
2022-02-16 08:43:00 -0600

    Trial 1
    Made a better code to complete all objectives

--- Python 412 Lab 1.py
+++
@@ -1,9 +0,0 @@
-print('How many total requests were made in the time period represented by the log?')
-
-logfile="http_access_log.txt"
-linescount=0
-with open (logfile,'r') as files:
-    for i in files:
-        linescount=linescount+1
-print(linescount)
-
